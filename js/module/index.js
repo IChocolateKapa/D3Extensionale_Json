65f799ef8eccb859678d7af18d636f6d1425bb3d
IChocolateKapa <echochocolatekapa@163.com>
2015-08-30 22:26:03 +0800

    test

---
+++ js/module/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Echo on 2015/8/30.
+ */
+
+require.config({
+    paths: {
+        "jquery": "../lib/jquery-2.1.3.min",
+        "d3": "../lib/d3.min"
+    }
+})
+
+require(['jquery', 'd3'], function($, d3){
+
+    //test turns out good
+    var svg = d3.select("body").append("svg");
+    svg.append("rect");
+})
